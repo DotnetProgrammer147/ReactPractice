10ede44992be9a10a01ce26fe47b59ff5b5fd637
user <user@abc.com>
2020-12-09 15:19:06 +0500

    add web api folder as well

---
+++ test-api/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using test_api.Models;
+
+namespace test_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {        
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Test Api Get Function From Test Controller";
+        }
+
+        [HttpGet]
+        public IActionResult Login([FromBody] Login login)
+        {
+            return Ok(login);
+        }
+    }
+}
