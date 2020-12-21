cb58263f4f696e381c89be42ff4ef9da303eba1f
user <user@abc.com>
2020-12-21 10:45:35 +0500

    updated

--- demo-app/src/Components/ApiLoginTest.js
+++ demo-app/src/Components/ApiLoginTest.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-
 class ApiLoginTest extends React.Component {
-
     
     constructor() {
-
         super();
         this.state = {
             Email: '',
@@ -30,39 +27,20 @@ class ApiLoginTest extends React.Component {
     }
 
     login(event) {
-
         //debugger;
-
         fetch('https://localhost:5001/api/Test/Login', {
-
             method: 'post',
-
             headers: {
-
                 'Accept': 'application/json',
-
                 'Content-Type': 'application/json'
-
             },
-
             body: JSON.stringify({
-
                 Email: this.state.Email,
-
                 Password: this.state.Password
-
             })
-
         }).then((Response) => Response.json())
-
             .then((result) => {
-
                 console.log(result);
-                //if (result.Status == 'Invalid')
-                 //   alert('Invalid User');
-                //else
-                    //this.props.history.push("/Dashboard");
-
             })
 
     }
@@ -70,25 +48,25 @@ class ApiLoginTest extends React.Component {
     render(){
         return <div style={{marginTop:100}}>
             <form id="loginForm" method="submit" >
-            <div className="col-md-6 offset-md-3">
-                <h3>
-                    <i className="fa fa-sign-in"></i>
-                    <span> Sign In</span>
-                </h3>
-            </div>
-            <div className="col-md-4 offset-md-4">            
-                <div className="row form-group">
-                    <label className="control-label">Email</label>
-                    <input type='text' className="form-control" onChange={this.Email} placeholder="Email" />
+                <div className="col-md-6 offset-md-3">
+                    <h3>
+                        <i className="fa fa-sign-in"></i>
+                        <span> Sign In</span>
+                    </h3>
                 </div>
-                <div className="row form-group">
-                    <label className="control-label">Password</label>
-                    <input type='Password' className="form-control" onChange={this.Password} placeholder="Password" />
+                <div className="col-md-4 offset-md-4">            
+                    <div className="row form-group">
+                        <label className="control-label">Email</label>
+                        <input type='text' className="form-control" onChange={this.Email} placeholder="Email" />
+                    </div>
+                    <div className="row form-group">
+                        <label className="control-label">Password</label>
+                        <input type='Password' className="form-control" onChange={this.Password} placeholder="Password" />
+                    </div>
+                    <div className="row form-group">
+                        <button type="button" className="btn btn-success" onClick={this.login}> Submit</button>
+                    </div>
                 </div>
-                <div className="row form-group">
-                    <button className="btn btn-success" onClick={this.login}> Submit</button>
-                </div>
-            </div>
             </form>
         </div>
     }
--- demo-app/src/Components/ApiLoginTestNew.js
+++ demo-app/src/Components/ApiLoginTestNew.js
@@ -5,10 +5,7 @@ import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGr
 class ApiLoginTestNew extends Component {
 
     constructor() {
-
         super();
-
-
         this.state = {
             Email: '',
             Password: ''
@@ -32,113 +29,58 @@ class ApiLoginTestNew extends Component {
     }
 
     login(event) {
-
         //debugger;
-
         fetch('https://localhost:5001/api/Test/LoginNew', {
-
             method: 'post',
-
             headers: {
-
                 'Accept': 'application/json',
-
                 'Content-Type': 'application/json'
-
             },
-
             body: JSON.stringify({
-
                 Email: this.state.Email,
-
                 Password: this.state.Password
-
             })
-
         }).then((Response) => Response.json())
-
             .then((result) => {
-
                 console.log(result);
                 //if (result.Status == 'Invalid')
                  //   alert('Invalid User');
                 //else
                     //this.props.history.push("/Dashboard");
-
             })
-
     }
-
 
     render() {
-
-
         return (
-
             <div className="app flex-row align-items-center">
-
                 <Container>
-
                     <Row className="justify-content-center">
-
                         <Col md="9" lg="7" xl="6">
-
-
                             <CardGroup>
-
                                 <Card className="p-2">
-
                                     <CardBody>
-
                                         <Form>
-
                                             <div className="row mb-2 pageheading">
-
                                                 <div className="col-sm-12 btn btn-primary">
-
                                                     Login
-
-                             </div>
-
+                                                </div>
                                             </div>
-
                                             <InputGroup className="mb-3">
-
-
                                                 <Input type="text" onChange={this.Email} placeholder="Enter Email" />
-
                                             </InputGroup>
-
                                             <InputGroup className="mb-4">
-
-
                                                 <Input type="password" onChange={this.Password} placeholder="Enter Password" />
-
                                             </InputGroup>
-
                                             <Button onClick={this.login} color="success" block>Login</Button>
-
                                         </Form>
-
                                     </CardBody>
-
                                 </Card>
-
                             </CardGroup>
-
                         </Col>
-
                     </Row>
-
                 </Container>
-
             </div>
-
         );
-
     }
-
 }
-
-
 export default ApiLoginTestNew;
