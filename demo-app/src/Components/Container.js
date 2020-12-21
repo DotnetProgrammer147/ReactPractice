e200725a45d2e268f152ed6b2aeec36717288dcb
user <user@abc.com>
2020-12-21 11:03:29 +0500

    components re-adjusted

--- demo-app/src/Components/Container.js
+++ demo-app/src/Components/Container.js
@@ -3,14 +3,13 @@ import Car from './Car';
 import Shape from './Shape';
 import SignIn from './SignIn';
 import ApiStringTest from './ApiStringTest';
-import ApiLoginTest from './ApiLoginTest';
 import ApiLoginTestNew from './ApiLoginTestNew'
 
 class Container extends React.Component{
 
     render(){
-        return <div>
-            <ApiLoginTest />
+        return <div>            
+            <SignIn />
         </div>
     }
 }
